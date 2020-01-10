3c1bfe210bb1b376ee4016c53197242a8088e7f6
Jhosefer Senna <jhosefer_nf@hotmail.com>
2020-01-10 00:19:59 -0300

    Adicionado Detalhes dos produtos

--- site.php
+++ site.php
@@ -34,3 +34,13 @@ $app->get("/categories/:idcategory", function ($idcategory) {
 		'pages' => $pages,
 	]);
 });
+
+$app->get('/products/:desurl', function ($desurl) {
+	$product = new Product();
+	$product->getFromUrl($desurl);
+	$page = new Page();
+	$page->setTpl('product-detail', [
+		'product' => $product->getValues(),
+		'categories' => $product->getCategories()
+	]);
+});
